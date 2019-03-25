67f780c480e3726f9c7813f0f4f35050b0f1928f
hasan genc <hasangencx@hotmail.com>
2019-03-25 17:08:03 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='chaos',
+      version='1.0.0',
+      description='Chaos theory visualizations',
+      url='https://github.com/hasangenc0/chaos',
+      keywords = "chaos fractals",
+      author='hasangenc0',
+      license='MIT',
+      packages=find_packages(),
+      )
